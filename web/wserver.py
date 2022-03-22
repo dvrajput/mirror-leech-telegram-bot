3712e238fbde670ce738e6ceb4e9ecbec30f0389
dvrajput <86516265+dvrajput@users.noreply.github.com>
2022-03-22 11:55:08 +0530

    Update wserver.py

--- web/wserver.py
+++ web/wserver.py
@@ -226,8 +226,8 @@ input[type="submit"]:hover, input[type="submit"]:focus{
         </a>
       </div>
       <div class="social">
-        <a href="https://www.github.com/anasty17/mirror-leech-telegram-bot"><i class="fab fa-youtube"></i></a>
-        <a href="https://t.me/dvmoviesbackupr"><i class="fab fa-telegram"></i></a>
+        <a href="https://t.me/profesar_bot"><i class="far fa-user-circle"></i></a>
+        <a href="https://t.me/dvmoviesbackup"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <div id="sticks">
@@ -613,13 +613,13 @@ section span{
           src="https://telegra.ph/file/cc06d0c613491080cc174.png"
           alt="logo"
         />
-        <a href="https://t.me/anas_tayyar">
+        <a href="https://t.me/profesar_bot">
           <h2 class="name">Qbittorrent Selection</h2>
         </a>
       </div>
       <div class="social">
-        <a href="https://www.github.com/anasty17/mirror-leech-telegram-bot"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/anas_tayyar"><i class="fab fa-telegram"></i></a>
+        
+        <a href="https://t.me/dvmoviesbackup"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <section>
@@ -754,7 +754,7 @@ def set_priority(hash_id):
 
 @app.route('/')
 def homepage():
-    return "<h1>Checkout Our Official <a href='https://t.me/dvmoviesbackup'>DV Movies Network</a> & Join & Share & Support To us</h1>"
+    return "<h1>Checkout Our Official <a href='https://t.me/+NOeQODLI8J44ODI1'>DV Movies Network</a> & Join & Share & Support To us</h1>"
 
 @app.errorhandler(NotFound404Error)
 def page_not_found(e):
